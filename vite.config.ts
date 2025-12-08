5ded7739d0abd0edea4a6f5fdac95f855c3d5f79
Francesco Molinari <molinarifrancesco17@gmail.com>
2025-12-08 21:11:32 +0100

    github pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig(config => ({
+  base: '/biliardino-elo/',
   publicDir: false,
   resolve: {
     alias: {
