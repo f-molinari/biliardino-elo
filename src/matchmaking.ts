ac1f28de96849edc6a48d8440fd26d2f939f0f19
Francesco Molinari <molinarifrancesco17@gmail.com>
2026-01-01 18:47:05 +0100

    refactor: auth util

--- src/matchmaking.ts
+++ src/matchmaking.ts
@@ -1,52 +1,9 @@
-import { browserSessionPersistence, onAuthStateChanged, setPersistence } from 'firebase/auth';
 import './pwa';
-import { AUTH, login } from './utils/firebase.util';
+import { withAuthentication } from './utils/auth.util';
 import { MatchmakingView } from './views/matchmaking.view';
 
-async function init(user: any): Promise<void> {
-  if (!user) throw new Error('Not authenticated');
+async function init(): Promise<void> {
   MatchmakingView.init();
 }
 
-async function showLoginDialog(): Promise<void> {
-  const dialog = document.getElementById('loginDialog') as HTMLDialogElement;
-  const form = document.getElementById('loginForm') as HTMLFormElement;
-
-  dialog.showModal();
-
-  return new Promise((resolve, reject) => {
-    const onCancel = (): void => reject(new Error('Login cancelled'));
-
-    form.onsubmit = async (e) => {
-      e.preventDefault();
-
-      const email = (document.getElementById('loginEmail') as HTMLInputElement).value;
-      const password = (document.getElementById('loginPassword') as HTMLInputElement).value;
-
-      try {
-        await setPersistence(AUTH, browserSessionPersistence);
-        await login(email, password);
-        dialog.close();
-        resolve();
-      } catch {
-        alert('Invalid username or password');
-      }
-    };
-
-    dialog.addEventListener('cancel', onCancel, { once: true });
-  });
-}
-
-let started = false;
-
-onAuthStateChanged(AUTH, async (user) => {
-  if (started) return;
-
-  if (!user || started) {
-    await showLoginDialog();
-    return;
-  }
-
-  started = true;
-  await init(user);
-});
+withAuthentication(init);
